2f8ed8b570e641be32a363c880fa987ab3d6e951
vlvgfx <96151706+vlvgfx@users.noreply.github.com>
2021-12-18 13:41:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>My Digital Garden!</title>
+        <title>VILAV BHATT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
